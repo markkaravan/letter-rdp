e9917a463e98ac3577db19684815ab813f2704bc
Mark Karavan <mkaravan@Marks-MacBook-Pro.local>
2021-12-19 21:56:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Letter Recursive Descent Parser
+Based on [Eva VM course](https://www.dmitrysoshnikov.education/p/parser-from-scratch) by [Dmitry Soshnikov](https://www.dmitrysoshnikov.education/).
---
+++ src/Parser.js
@@ -0,0 +1,36 @@
+/**
+* Letter parser: recursive descent implementation
+*/
+
+class Parser {
+  /**
+  *   Parses a string into an AST
+  */
+  parse(string) {
+    this._string = string;
+
+
+    return this.Program();
+  }
+
+  /**
+  *
+  */
+  Program() {
+    return this.NumericLiteral();
+  }
+
+  /**
+  *
+  */
+  NumericLiteral() {
+    return {
+      type: 'NumericLiteral',
+      value: Number(this._string),
+    };
+  }
+}
+
+module.exports = {
+  Parser,
+}
