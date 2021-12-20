4a063eab143cb72a5ebb2d8090ef397694504b1b
Mark Karavan <mkaravan@Marks-MacBook-Pro.local>
2021-12-20 14:45:20 -0500

    adds statements and TDD

--- __tests__/run.js
+++ __tests__/run.js
@@ -1,15 +1,44 @@
 const {Parser} = require('../src/Parser');
+const assert = require('assert');
+
+/**
+*   List of tests
+*/
+const tests = [
+  require('./literals-test.js'),
+  require('./statement-list-test.js')
+];
 
 const parser = new Parser();
 
-const program = `
-    /**
-    *   Documentation comment
-    */
+function exec() {
+  const program = `
+      /**
+      *   Documentation comment
+      */
 
-    46
-`;
+      "hello";
 
-const ast = parser.parse(program);
+      69;
+  `;
 
-console.log(JSON.stringify(ast, null, 2));
+  const ast = parser.parse(program);
+
+  console.log(JSON.stringify(ast, null, 2));
+}
+
+// Manual test
+//exec();
+
+/**
+*   Test function
+*/
+function test(program, expected) {
+  const ast = parser.parse(program);
+  assert.deepEqual(ast, expected);
+}
+
+// Run all tests
+tests.forEach(testRun => testRun(test));
+
+console.log("All assertions passed");
